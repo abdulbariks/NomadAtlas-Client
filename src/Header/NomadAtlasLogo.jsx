381d4cf073036c1dafa836877b9de696fb278e68
Abdul Barik <abdulbarik1997m@gmail.com>
2025-09-19 03:13:38 +0600

    Updates

--- src/Header/NomadAtlasLogo.jsx
+++ src/Header/NomadAtlasLogo.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
+import React from "react";
 import Lottie from "lottie-react";
-import { Link } from 'react-router';
-import logo from "../assets/Logo/NomadLogo.png"
+import { Link } from "react-router";
+import logo from "../assets/Logo/NomadLogo.png";
 
 const NomadAtlasLogo = () => {
-    return (
+  return (
+    <Link to="/">
+      <div className="flex gap-1 items-center">
+        {/* Lottie Animation */}
 
-        
-        <Link to="/">
-        <div className="flex gap-1 items-center">
-    {/* Lottie Animation */}
-
-            {/* <Lottie
+        {/* <Lottie
             animationData={logo}
             loop={true}
             className="size-10 md:size-14 "  
@@ -19,18 +17,17 @@ const NomadAtlasLogo = () => {
           
             </Lottie> */}
 
-  {/* Logo */}
+        {/* Logo */}
 
-        <img src={logo} alt="logo"  className='size-5'/>
+        <img src={logo} alt="logo" className="size-5" />
 
-          {/* Text */}   
-             <h2 className=' text-md md:text-xl font-extrabold   text-gray-900'>
-            NomadAtlas
-           </h2>
-        </div>
-        </Link>
-       
-    );
+        {/* Text */}
+        <h2 className=" text-md md:text-xl font-extrabold   text-gray-900">
+          NomadAtlas
+        </h2>
+      </div>
+    </Link>
+  );
 };
 
 export default NomadAtlasLogo;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import Banner from "../Component/Banner/Banner";
 import Reviews from "../components/Home/Reviews";
 import Milestones from "../components/Home/Milestones";
 import Newsletter from "../components/Home/Newsletter";
 import CityDiscovery from "../components/Home/CityDiscovery";
 import ComminityPosts from "../components/Home/ComminityPosts";
-import PopularDestination from "../kousar/components/popularDestination/popularDestination";
-import NomadFavouriteDestination from "../kousar/components/nomadFavouriteDestination/nomadFavouriteDestination";
+import Banner from "../Header/Banner";
+import PopularDestination from "../components/Home/PopularDestination";
+import NomadFavouriteDestination from "../components/Home/FavouriteDestination";
 
 const Home = () => {
   return (
     <div>
       <Banner />
-      <PopularDestination></PopularDestination>
-      <NomadFavouriteDestination></NomadFavouriteDestination>
       <CityDiscovery />
       <ComminityPosts />
+      <PopularDestination />
+      <NomadFavouriteDestination />
       <Reviews />
       <Milestones />
       <Newsletter />
