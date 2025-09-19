62d9b2aa2f06caa3740c3d15ab13040841861d98
questcoderull <questcoderull@gmail.com>
2025-09-19 11:47:06 +0600

    added functionality to select a country

--- src/pages/HomeComponents/SmartCostPreview.jsx
+++ src/pages/HomeComponents/SmartCostPreview.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const data = [
   {
@@ -26,14 +26,47 @@ const data = [
 ];
 
 const SmartCostPreview = () => {
+  const [selectedCountry, setSelectedCountry] = useState(null);
   return (
-    <div>
-      <h1>Hi it's from smart cost calculator</h1>
+    <div className="max-w-7xl mx-auto">
+      <h1 className="text-2xl font-bold text-center">
+        Smar Cost Calculator preview
+      </h1>
 
-      {/* Budget input */}
-      <div>
-        <label>Enter your full budget</label>
-        <input type="number" name="budget" id="" />
+      {/* Budget Input */}
+      <div className="mb-4">
+        <label className="block font-medium text-lg text-gray-700 mb-1 ">
+          Enter your total budget ($)
+        </label>
+        <input
+          type="number"
+          placeholder="e.g. 5000"
+          className="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-400"
+        />
+      </div>
+
+      {/* Country Select */}
+      <div className="mb-4">
+        <p className="block text-2xl font-bold text-center text-gray-700 mb-1">
+          Select a country
+        </p>
+
+        {/* Option to select a country */}
+        <div className="flex flex-wrap gap-3 mb-6 justify-center">
+          {data.map((country) => (
+            <button
+              key={country.country}
+              onClick={() => setSelectedCountry(country.country)}
+              className={`px-4 py-2 rounded-full border transition-all cursor-pointer ${
+                selectedCountry === country.country
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-100 text-gray-800 hover:bg-blue-100"
+              }`}
+            >
+              {country.country}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
