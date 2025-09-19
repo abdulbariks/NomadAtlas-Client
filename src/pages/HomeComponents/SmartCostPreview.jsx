4acd2a9d4c58a5cd830e2259a894adcdab05d207
questcoderull <questcoderull@gmail.com>
2025-09-19 12:17:03 +0600

    added functionality to select a city of selected country

--- src/pages/HomeComponents/SmartCostPreview.jsx
+++ src/pages/HomeComponents/SmartCostPreview.jsx
@@ -27,6 +27,15 @@ const data = [
 
 const SmartCostPreview = () => {
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  const selectedCountryData = data.find(
+    (countryData) => countryData.country == selectedCountry
+  );
+  // console.log(selectedCountryData);
+  const citiesOfCountry = selectedCountryData?.cities || [];
+  // console.log(citiesOfCountry);
+
   return (
     <div className="max-w-7xl mx-auto">
       <h1 className="text-2xl font-bold text-center">
@@ -36,7 +45,7 @@ const SmartCostPreview = () => {
       {/* Budget Input */}
       <div className="mb-4">
         <label className="block font-medium text-lg text-gray-700 mb-1 ">
-          Enter your total budget ($)
+          Enter your budget jawa ashar koroch. ($)
         </label>
         <input
           type="number"
@@ -64,6 +73,28 @@ const SmartCostPreview = () => {
               }`}
             >
               {country.country}
+            </button>
+          ))}
+        </div>
+
+        {/* Option to select a City */}
+        <p className="block text-2xl font-bold text-center text-gray-700 mb-1">
+          Select a City of{" "}
+          <span className="text-blue-500">{selectedCountry}</span>
+        </p>
+
+        <div className="flex flex-wrap gap-3 mb-6 justify-center">
+          {citiesOfCountry.map((city) => (
+            <button
+              key={city.name}
+              onClick={() => setSelectedCity(city.name)}
+              className={`px-4 py-2 rounded-full border transition-all cursor-pointer ${
+                selectedCity === city.name
+                  ? "bg-blue-500 text-white"
+                  : "bg-gray-100 text-gray-800 hover:bg-blue-100"
+              }`}
+            >
+              {city.name}
             </button>
           ))}
         </div>
