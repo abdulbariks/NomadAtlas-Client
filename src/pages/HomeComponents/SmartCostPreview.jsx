6e43c6583e4985a8191a41748baf232abe40ae9e
questcoderull <questcoderull@gmail.com>
2025-09-19 15:37:23 +0600

    functionality for shoiwng how many days user can stay in any city are implemented

--- src/pages/HomeComponents/SmartCostPreview.jsx
+++ src/pages/HomeComponents/SmartCostPreview.jsx
@@ -29,12 +29,20 @@ const SmartCostPreview = () => {
   const [selectedCountry, setSelectedCountry] = useState("Germany");
   const [selectedCity, setSelectedCity] = useState(null);
 
+  const [budget, setBudget] = useState(0);
+  // console.log(budget);
+
   const selectedCountryData = data.find(
     (countryData) => countryData.country == selectedCountry
   );
   // console.log(selectedCountryData);
   const citiesOfCountry = selectedCountryData?.cities || [];
   // console.log(citiesOfCountry);
+
+  // how may days he can stay.
+  const days = selectedCity
+    ? Math.floor(budget / (selectedCity.livingCost / 30))
+    : 0;
 
   return (
     <div className="max-w-7xl mx-auto">
@@ -49,7 +57,9 @@ const SmartCostPreview = () => {
         </label>
         <input
           type="number"
-          placeholder="e.g. 5000"
+          name="budgetInput"
+          onChange={(e) => setBudget(e.target.value)}
+          placeholder="Example: 5000"
           className="w-full border rounded px-3 py-2 focus:ring-2 focus:ring-blue-400"
         />
       </div>
@@ -90,9 +100,9 @@ const SmartCostPreview = () => {
               {citiesOfCountry.map((city) => (
                 <button
                   key={city.name}
-                  onClick={() => setSelectedCity(city.name)}
+                  onClick={() => setSelectedCity(city)}
                   className={`px-4 py-2 rounded-full border transition-all cursor-pointer ${
-                    selectedCity === city.name
+                    selectedCity?.name === city.name
                       ? "bg-blue-500 text-white"
                       : "bg-gray-100 text-gray-800 hover:bg-blue-100"
                   }`}
@@ -109,15 +119,16 @@ const SmartCostPreview = () => {
           {/* result */}
           <div className="bg-blue-50 border border-blue-200 p-4 rounded-lg text-center">
             <h3 className="text-2xl font-bold mb-2">
-              Result for <span className="text-green-600">{selectedCity}</span>{" "}
-              , <span className="text-blue-600">{selectedCountry}</span>
+              Result for{" "}
+              <span className="text-green-600">{selectedCity?.name}</span> ,{" "}
+              <span className="text-blue-600">{selectedCountry}</span>
             </h3>
             <p className="text-gray-700">
               With a budget of{" "}
-              <span className="font-bold text-green-600">$898</span>, you can
-              stay about{" "}
-              <span className="font-bold text-blue-600">10 days</span> in{" "}
-              {selectedCity}.
+              <span className="font-bold text-green-600">${budget}</span>{" "}
+              dollers, you can stay about{" "}
+              <span className="font-bold text-blue-600">{days} days</span> in{" "}
+              {selectedCity?.name}.
             </p>
           </div>
 
