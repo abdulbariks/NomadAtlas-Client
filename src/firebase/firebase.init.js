b5dc824bb1ccac582ee4bba0cf53aa1a69245f8f
Abdul Barik <abdulbarik1997m@gmail.com>
2025-09-26 03:30:46 +0600

    Authentication firebase with redux done

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,6 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +28,21 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+export const googleProvider = new GoogleAuthProvider();
+
+// Helper wrappers (optional)
+export const firebaseRegister = (email, password) =>
+  createUserWithEmailAndPassword(auth, email, password);
+
+export const firebaseLogin = (email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
+
+export const firebaseGoogleSignIn = () => signInWithPopup(auth, googleProvider);
+
+export const firebaseSendPasswordReset = (email) =>
+  sendPasswordResetEmail(auth, email);
+
+export const firebaseLogout = () => signOut(auth);
+
+export const firebaseOnAuthStateChanged = (cb) => onAuthStateChanged(auth, cb);
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,10 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import AboutUs from "../pages/AboutUs";
 import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
+import ProtectedRoute from "./ProtectedRoute";
+import Blogs from "../pages/Blogs";
+import CostCalculator from "../pages/CostCalculator";
 
 export const router = createBrowserRouter([
   {
@@ -25,6 +29,26 @@ export const router = createBrowserRouter([
       {
         path: "register",
         Component: Register,
+      },
+      {
+        path: "forgot-password",
+        Component: ForgotPassword,
+      },
+      {
+        path: "blogs",
+        Component: Blogs,
+      },
+      {
+        path: "cost-calculator",
+        Component: CostCalculator,
+      },
+      {
+        path: "cost-calculator",
+        element: (
+          <ProtectedRoute>
+            <Home></Home>
+          </ProtectedRoute>
+        ),
       },
       {
         path: "*",
